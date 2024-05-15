08e1f25c2273a233f736a3007539ed8eb54e542c
jtmcdermott9 <90660038+jtmcdermott9@users.noreply.github.com>
2024-05-15 18:21:01 -0500

    Added link to github repo to site

--- pages/about.py
+++ pages/about.py
@@ -11,6 +11,7 @@ layout = html.Div(children=[
                   html.P("BlightWatch NOLA was created by Eugene Lim and Joseph McDermott, both Tulane students who are"
                          " Econimics and Computer Science double majors. The project was created under the supervision"
                          " of Professor Nicholas Mattei. The project features contributions from Tulane student Austin Nguyen,"
-                         " who had to leave the project due to health problems. ")
+                         " who had to leave the project due to health problems. The source code is available at the link below."),
+                  html.A("BlightWatch-NOLA GitHub Repository", href="https://github.com/jtmcdermott9/BlightWatch-NOLA")
                 ]
                   )
